9027c80c359a6a2cf6425f2d7821e0e18418de36
MaharaniWahyuTantri <maharaniwahyutantri525@gmail.com>
2024-06-06 19:34:21 +0700

    nmafd

--- src/koneksi.php
+++ src/koneksi.php
@@ -6,7 +6,7 @@ $dbname = "uapweb";
 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
-
+//ha
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
