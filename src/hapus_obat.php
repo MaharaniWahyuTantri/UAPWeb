9136ccbb40a66dcf508ac9a5adcd389f97d2c47b
Firmansyahabdl <abdulfirmansyah456@gmail.com>
2024-06-06 20:23:09 +0700

    fix code

--- src/hapus_obat.php
+++ src/hapus_obat.php
@@ -5,7 +5,7 @@ $query = "DELETE FROM obat WHERE id=$obat_id";
 
 mysqli_query($conn, $query);
 if ($conn->query($query) === TRUE) {
-    header('Location: kocakobat.php');
+    header('Location: obat.php');
     exit;
 } else {
     echo "Error: " . $query . "<br>" . $conn->error;
